1a830985197dcc9d1b66c5e38d796369adee1611
Eray Ogün Özdemir <90385007+Jeglenea@users.noreply.github.com>
2023-12-04 09:56:48 +0300

    Update constant.js

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,9 +1,9 @@
 export const logoURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png';
 
-export const POPULAR_API_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=986fd9a832dc29e418d6705c077923df&language=en-US';
-export const UPCOMING_API_URL = 'https://api.themoviedb.org/3/movie/upcoming?api_key=986fd9a832dc29e418d6705c077923df&language=en-US';
-export const TOPRATED_API_URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=986fd9a832dc29e418d6705c077923df&language=en-US';
-export const NOWPLAYING_API_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=986fd9a832dc29e418d6705c077923df&language=en-US';
+export const POPULAR_API_URL = 'MovieDB-API Key';
+export const UPCOMING_API_URL = 'MovieDB-API Key';
+export const TOPRATED_API_URL = 'MovieDB-API Key';
+export const NOWPLAYING_API_URL = 'MovieDB-API Key';
 
 export const type = {
     popular: 'Most Popular',
